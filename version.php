3919af1d54f7c032a94f8d8c9adde8dd9bceb65a
sokunthearithmakara <smakara@hawaii.edu>
2026-08-13 18:42:45 +0700

    Bump version requirement

--- version.php
+++ version.php
@@ -25,13 +25,13 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component    = 'local_ivpdfviewer';
-$plugin->release      = '1.4';
-$plugin->version      = 2026060800;
+$plugin->release      = '1.4.1';
+$plugin->version      = 2026080500;
 $plugin->requires     = 2021112800;
 $plugin->supported    = [400, 502];
 $plugin->maturity     = MATURITY_STABLE;
 $plugin->dependencies = [
-    'interactivevideo' => 2026060800,
+    'interactivevideo' => 2026080103,
     'ivplugin_richtext' => 2024071500,
     'ivplugin_iframe' => 2024071500,
 ];
